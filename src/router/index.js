1b2dc671f38c7d0ef8db2ba018c1402bd0eb3192
xmun74 <xmun74@naver.com>
2023-11-15 16:40:52 +0900

    chore: router 폴더 적용

---
+++ src/router/index.js
@@ -0,0 +1,39 @@
+import VueRouter from "vue-router";
+import Vue from "vue";
+import Home from "@/pages/Home";
+import PostDetail from "@/pages/PostDetail";
+import CreatePost from "@/pages/CreatePost";
+import UpdatePost from "@/pages/UpdatePost";
+import ErrorPage from "@/pages/ErrorPage";
+
+Vue.use(VueRouter);
+
+const routes = [
+  {
+    path: "/",
+    component: Home,
+  },
+  {
+    path: "/createPost",
+    component: CreatePost,
+  },
+  {
+    path: "/post/:id",
+    component: PostDetail,
+  },
+  {
+    path: "/update/:id",
+    component: UpdatePost,
+  },
+  {
+    path: "*",
+    component: ErrorPage,
+  },
+];
+
+const router = new VueRouter({
+  mode: "history",
+  routes,
+});
+
+export default router;
